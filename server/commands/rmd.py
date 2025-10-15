2e2d74989471f94e73d5233a5f74b948d8a31d31
kmvega-05 <kmvega-0547@gmail.com>
2025-10-14 22:02:47 -0400

    refact(server): Refactorized MKD and RMD commands

--- server/commands/rmd.py
+++ server/commands/rmd.py
@@ -1,26 +1,26 @@
-from entities.file_system import remove_directory, get_user_root_directory
+from entities.file_system_manager import remove_directory, get_user_root_directory
 
 def handle_rmd(command, client_socket, server, client_session):
     """Maneja comando RMD - Remove Directory"""
-    if not client_session.is_authenticated():
-        server.send_response(client_socket, 530, "Not logged in")
-        return
-
+    
+    # Chequea argumentos
     if not command.require_args(1):
         server.send_response(client_socket, 501, "Syntax error in parameters")
         return
 
+    # Valida autenticación
+    if not client_session.is_authenticated():
+        server.send_response(client_socket, 530, "Not logged in")
+        return
+
     dir_name = command.get_arg(0)
+
+    # Obtener directorio raíz y actual(para resolver rutas absolutas/relativas)
     user_root = get_user_root_directory(client_session.username)
+    current_directory = client_session.get_current_directory()
     
-    # Crear la ruta completa relativa al directorio actual
-    if dir_name.startswith('/'):
-        dir_path = dir_name
-    else:
-        dir_path = client_session.current_directory + '/' + dir_name
-    
-    # Eliminar el directorio
-    success, message = remove_directory(user_root, dir_path)
+    # Eliminar directorio
+    success, message = remove_directory(user_root, current_directory, dir_name)
     
     if success:
         server.send_response(client_socket, 250, message)
