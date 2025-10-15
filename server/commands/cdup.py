c6b50deba39f8a0403f70d6ee520539f160f7374
kmvega-05 <kmvega-0547@gmail.com>
2025-10-14 21:06:22 -0400

    doc(server) : added documentation to CWD, CDUP and PWD commands

--- server/commands/cdup.py
+++ server/commands/cdup.py
@@ -2,22 +2,26 @@ from entities.file_system_manager import change_directory, get_user_root_directo
 
 def handle_cdup(command, client_socket, server, client_session):
     """Maneja comando CDUP - Change to Parent Directory"""
+
+    # Chequear argumentos
+    if not command.require_args(0):
+        server.send_response(client_socket, 501, "Syntax error in parameters")
+        return
+    
+    # Verificar autenticación 
     if not client_session.is_authenticated():
         server.send_response(client_socket, 530, "Not logged in")
         return
 
-    if not command.require_args(0):
-        server.send_response(client_socket, 501, "Syntax error in parameters")
-        return
-
+    # Obtener directorio raiz y actual (para resolver rutas absolutas/relativas)
     user_root = get_user_root_directory(client_session.username)
     current_dir = client_session.get_current_directory()
     
-    # Cambiar al directorio padre
+    # Determinar directorio padre
     new_current_dir = change_directory(user_root, current_dir, "..")
     
     if new_current_dir:
-        client_session.current_directory = new_current_dir
+        client_session.current_directory = new_current_dir # actualizar en sesión
         server.send_response(client_socket, 200, f'Directory changed to parent directory "{new_current_dir}"')
     else:
         server.send_response(client_socket, 550, "Failed to change to parent directory")
--- server/commands/cwd.py
+++ server/commands/cwd.py
@@ -2,22 +2,28 @@ from entities.file_system_manager import get_user_root_directory, change_directo
 
 def handle_cwd(command, client_socket, server, client_session):
     """Maneja comando CWD - Change Working Directory"""
+
+    # Chequear argumentos
+    if not command.require_args(1):
+        server.send_response(client_socket, 501, "Syntax error in parameters")
+        return
+    
+    # Verificar autenticación
     if not client_session.is_authenticated():
         server.send_response(client_socket, 530, "Not logged in")
         return
 
-    if not command.require_args(1):
-        server.send_response(client_socket, 501, "Syntax error in parameters")
-        return
-
     new_directory = command.get_arg(0)
+    
+    # Obtener directorio raíz y actual(para resolver rutas absolutas/relativas)
     user_root_directory = get_user_root_directory(client_session.username)
     current_directory = client_session.get_current_directory()
     
+    # Determinar nuevo directorio
     new_current_dir = change_directory(user_root_directory, current_directory, new_directory)
     
     if new_current_dir:
-        client_session.current_directory = new_current_dir
+        client_session.current_directory = new_current_dir  # actualizar nuevo directorio en la sesión
         server.send_response(client_socket, 250, f'Directory changed to "{new_current_dir}"')
     else:
         server.send_response(client_socket, 550, "Failed to change directory")
