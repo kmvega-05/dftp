01e918944485c0f8d55dc009c3e55dd96233704f
kmvega-05 <kmvega-0547@gmail.com>
2025-10-14 22:02:47 -0400

    refact(server): Refactorized MKD and RMD commands

--- server/commands/mkd.py
+++ server/commands/mkd.py
@@ -1,26 +1,26 @@
-from entities.file_system import create_directory, get_user_root_directory
+from entities.file_system_manager import create_directory, get_user_root_directory
 
 def handle_mkd(command, client_socket, server, client_session):
     """Maneja comando MKD - Make Directory"""
+
+    # Chequea argumentos
+    if not command.require_args(1):
+        server.send_response(client_socket, 501, "Syntax error in parameters")
+        return
+    
+    # Valida autenticación
     if not client_session.is_authenticated():
         server.send_response(client_socket, 530, "Not logged in")
         return
 
-    if not command.require_args(1):
-        server.send_response(client_socket, 501, "Syntax error in parameters")
-        return
-
     new_dir_name = command.get_arg(0)
+    
+    # Obtener directorio raíz y actual(para resolver rutas absolutas/relativas)
     user_root = get_user_root_directory(client_session.username)
+    current_directory = client_session.get_current_directory()
     
-    # Crear la ruta completa relativa al directorio actual
-    if new_dir_name.startswith('/'):
-        dir_path = new_dir_name
-    else:
-        dir_path = client_session.current_directory + '/' + new_dir_name
-    
-    # Crear el directorio
-    success, message = create_directory(user_root, dir_path)
+    # Crea el directorio
+    success, message = create_directory(user_root, current_directory, new_dir_name)
     
     if success:
         server.send_response(client_socket, 257, message)
